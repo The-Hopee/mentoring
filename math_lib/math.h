26ad1b4cdf85713ab1b68ec14a511753c4f349c3
Timofey <timofej.derevyankin@inbox.ru>
2026-05-01 12:59:37 +0300

    add math lib correctly

--- math_lib/math.h
+++ math_lib/math.h
@@ -1,19 +1,50 @@
 #pragma once
+#include <climits>
 
 namespace math
 {
     inline int add(int a, int b)
     {
+        // Проверка переполнения при сложении
+        if (b > 0 && a > INT_MAX - b)
+        {
+            throw "Integer overflow: addition result exceeds INT_MAX.";
+        }
+        if (b < 0 && a < INT_MIN - b)
+        {
+            throw "Integer overflow: addition result is less than INT_MIN.";
+        }
         return a + b;
     }
 
     inline int subtract(int a, int b)
     {
+        // Проверка переполнения при вычитании
+        if (b > 0 && a < INT_MIN + b)
+        {
+            throw "Integer overflow: subtraction result is less than INT_MIN.";
+        }
+        if (b < 0 && a > INT_MAX + b)
+        {
+            throw "Integer overflow: subtraction result exceeds INT_MAX.";
+        }
         return a - b;
     }
 
     inline int multiply(int a, int b)
     {
+        // Проверка переполнения при умножении
+        if (a != 0 && b != 0)
+        {
+            if ((b > 0 && a > INT_MAX / b) || (b > 0 && a < INT_MIN / b))
+            {
+                throw "Integer overflow: multiplication result exceeds int limits.";
+            }
+            if ((b < 0 && a < INT_MAX / b) || (b < 0 && a > INT_MIN / b))
+            {
+                throw "Integer overflow: multiplication result exceeds int limits.";
+            }
+        }
         return a * b;
     }
 
@@ -36,6 +67,14 @@ namespace math
         int result = 1;
         for (int i = 0; i < b; i++)
         {
+            // Проверка переполнения при умножении
+            if (a != 0 && result != 0)
+            {
+                if ((a > 0 && result > INT_MAX / a) || (a < 0 && result < INT_MIN / a))
+                {
+                    throw "Integer overflow: pow result exceeds int limits.";
+                }
+            }
             result *= a;
         }
         return result;
@@ -53,6 +92,17 @@ namespace math
             return 1;
         }
 
-        return a * factorial(a - 1);
+        int prev = factorial(a - 1);
+        
+        // Проверка переполнения при умножении
+        if (a != 0 && prev != 0)
+        {
+            if (prev > INT_MAX / a)
+            {
+                throw "Integer overflow: factorial result exceeds int limits.";
+            }
+        }
+        
+        return a * prev;
     }
 }
