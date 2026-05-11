a106b77028eaf81c5435b83aa41555cdb26ac702
Timofey <timofej.derevyankin@inbox.ru>
2026-05-11 22:45:16 +0300

    branch for task

--- tt_2/main.cpp
+++ tt_2/main.cpp
@@ -1,6 +1,6 @@
 #include "interface.h"
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv) { // ./calculator '{"num1": 5, "num2": 3, "op": "x"}'
     std::string json_query = argc < 2 ? "" : argv[1];
     interface::app application;
     application.run(json_query);
